a81b7ba3ca9716d81ed7e78ac18ff1aee2369dd3
Catherine Burvelle <catherine@taodyne.com>
2013-08-12 12:16:41 +0200

    Merge branch 'master' of ssh://io.taodyne.com/~ddd/Work/tao

--- doc/web_remote.doxy.h
+++ doc/web_remote.doxy.h
@@ -1,15 +1,15 @@
 /**
  * @~english
  * @taomoduledescription{WebRemote, Web Remote Control}
- *
- * Control presentations from a web browser (computer, tablet, smartphone)
+ * <tt>import WebRemote</tt> - Control presentations from a web browser
+ * (computer, tablet, smartphone).@n
  *
  * @endtaomoduledescription{WebRemote}
  *
  * @~french
  * @taomoduledescription{WebRemote, Télécommande Web}
- *
- * Télécommandez vos présentations depuis un navigateur web (ordinateur, tablette, smartphone) 
+ * <tt>import WebRemote</tt> - Télécommandez vos présentations depuis un
+ * navigateur web (ordinateur, tablette, smartphone).@n
  *
  * @endtaomoduledescription{WebRemote}
  *
